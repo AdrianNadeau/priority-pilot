7eee2906001a6dbdf61bd76535554bf02b9ff863
jawad-ul-hassan <intelligentjawad@gmail.com>
2025-03-10 21:24:05 +0500

    error page added

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -4,10 +4,10 @@ const errorHandler = (err, req, res, next) => {
 
   const statusCode = err.statusCode || 500;
 
-  res.status(statusCode).json({
-    success: false,
+  res.render("Pages/pages-error", {
+    person: req.session.person,
     message: err.message || "Internal Server Error",
-    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+    statusCode,
   });
 };
 
