8f835420596a273c647519973ec2abf51b978e79
jawad-ul-hassan <intelligentjawad@gmail.com>
2025-03-15 01:01:46 +0500

    fixed error handler bug

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,10 +3,11 @@ const errorHandler = (err, req, res, next) => {
   console.error(`Error: ${err.message}`);
 
   const statusCode = err.statusCode || 500;
+  const errorMessage = err.message || "Internal Server Error";
 
   res.render("Pages/pages-error", {
     person: req.session.person,
-    errorMessage: err.message || "Internal Server Error",
+    errorMessage,
     statusCode,
   });
 };
